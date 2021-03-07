fcee7a31b45a176b1cc35e6f55741ad7c4ac7da5
evanbowman <ebowman@bu.edu>
2021-03-06 21:06:26 -0500

    refactor screen stuff

--- source/main.asm
+++ source/main.asm
@@ -5,11 +5,13 @@
 ;;; ----------------------------------------------------------------------------
 
         INCLUDE "hardware.inc"
+        INCLUDE "defs.inc"
 
 
 ;;; ############################################################################
 
         SECTION "MAIN", ROM0
+
 
 
 ;;; ----------------------------------------------------------------------------
@@ -18,12 +20,13 @@ Main:
         ld	a, IEF_VBLANK	        ; vblank interrupt
 	ld	[rIE], a	        ; setup
 
-        ld	a, LCDCF_ON | LCDCF_BG8000 | LCDCF_BG9800 | LCDCF_OBJ16 | LCDCF_WINOFF | LCDCF_BGON
+
+        ld	a, SCREEN_MODE
         ld	[rLCDC], a	        ; enable lcd
         ei
 
 .loop:
-        call    VBlankIntrWait
+        call    VBlankIntrWait          ; vsync
 
         jr      .loop
 
