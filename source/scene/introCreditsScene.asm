3b27b707300a1dde17c6639c3ca3d1d20e1386d6
evanbowman <ebowman@bu.edu>
2021-03-18 09:59:50 -0400

    commit stuff

---
+++ source/scene/introCreditsScene.asm
@@ -0,0 +1,83 @@
+;;; $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+;;;
+;;; ASM Source code for Red GBC, by Evan Bowman, 2021
+;;;
+;;;
+;;; The following licence covers the source code included in this file. The
+;;; game's characters and artwork belong to Evan Bowman, and should not be used
+;;; without permission.
+;;;
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;;
+;;; 1. Redistributions of source code must retain the above copyright notice,
+;;; this list of conditions and the following disclaimer.
+;;;
+;;; 2. Redistributions in binary form must reproduce the above copyright notice,
+;;; this list of conditions and the following disclaimer in the documentation
+;;; and/or other materials provided with the distribution.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
+;;; $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+
+;;; $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+;;;
+;;;
+;;;  Intro Credits Scene
+;;;
+;;;
+;;; $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+IntroCreditsSceneEnter:
+        call    VBlankIntrWait
+
+        call    LoadOverworldPalettes
+
+        ld      hl, OverlayTiles
+        ld      bc, OverlayTilesEnd - OverlayTiles
+        ld      de, $9000
+        call    VramSafeMemcpy
+
+        ld      de, IntroCreditsSceneUpdate
+        call    SceneSetUpdateFn
+
+        ld      a, 0
+        ld      [var_scene_counter], a
+
+        jp      UpdateFnResume
+
+
+;;; ----------------------------------------------------------------------------
+
+IntroCreditsSceneUpdate:
+        ld      a, [var_scene_counter]
+        inc     a
+        ld      [var_scene_counter], a
+        cp      127
+        jr      Z, .nextScene
+	jr      .done
+
+.nextScene:
+        ld      de, OverworldSceneEnter
+        call    SceneSetUpdateFn
+
+.done:
+        jp      UpdateFnResume
+
+
+;;; ----------------------------------------------------------------------------
